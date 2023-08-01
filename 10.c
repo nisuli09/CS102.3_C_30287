f2ff8858ec7d7c4355a2ff3d6581b3b26311b5e1
nisuli <135607283+nisuli09@users.noreply.github.com>
2023-08-01 13:18:20 +0530

    Add files via upload

---
+++ 10.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+int main ()
+{
+  char x;
+ printf("Enter your character ");
+ scanf("%c",&x);
+ switch(x)
+ {
+ case 'a':
+ printf("%c is a vowel",x);
+ break;
+ case 'A':
+ printf("%c is a vowel",x);
+ break;
+ case 'e':
+ printf("%c is a vowel",x);
+ break;
+ case 'E':
+ printf("%c is a vowel",x);
+ break;
+ case 'i':
+ printf("%c is a vowel",x);
+ break;
+ case 'I':
+ printf("%c is a vowel",x);
+ break;
+ case 'o':
+ printf("%c is a vowel",x);
+ break;
+ case 'O':
+ printf("%c is a vowel",x);
+ break;
+ case 'u':
+ printf("%c is a vowel",x);
+ break;
+ case 'U':
+ printf("%c is a vowel",x);
+ break;
+ default:
+ printf("%c is not a vowel",x);
+ }
+
+    return 0;
+}
+
+
+
+
